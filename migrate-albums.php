e44acfbb90b5d665f2ffa12111c9e9b646a6d6fd
mebels <36734239+mebels@users.noreply.github.com>
2018-03-12 08:56:45 +0100

    Update migrate-albums.php
    1. Database query extremely accelerated by WHERE = $value.
    2. Fix the sort_order bug. In Menalto, two albums can have the same "weight" (sort order number).
    3. An option for a limited test run built in (value $i with continue and break).

--- migrate-albums.php
+++ migrate-albums.php
@@ -13,14 +13,18 @@ exit("Connect failed: %s\n $mysqliconnecterror");
 
 $db_select_new_album = mysqli_query($dbconnect_new, "SELECT `id`,`folder`,`title`,`desc` FROM `albums`");
 
+//$i = 0;
 while($db_selectrows_new_album = mysqli_fetch_object($db_select_new_album)) {
+
+//$i++;
+//if ($i < 0) { continue; }
 
 $db_selectrows_new_album_id = $db_selectrows_new_album->id;
 $db_selectrows_new_album_folder = $db_selectrows_new_album->folder;
 $db_selectrows_new_album_title = $db_selectrows_new_album->title;
 $db_selectrows_new_album_desc = $db_selectrows_new_album->desc;
 
-$db_select_old_album = mysqli_query($dbconnect_old, "SELECT `album_cover_item_id`,`relative_path_cache`,`title`,`description`,`level` FROM `items` WHERE `type`='album'");
+$db_select_old_album = mysqli_query($dbconnect_old, "SELECT `album_cover_item_id`,`relative_path_cache`,`title`,`description`,`level` FROM `items` WHERE `relative_path_cache`='" . mysqli_real_escape_string($dbconnect_old, $db_selectrows_new_album_folder) . "'");
 
 while($db_selectrows_old_album = mysqli_fetch_object($db_select_old_album)) {
 
@@ -30,35 +34,40 @@ $db_selectrows_old_album_desc = $db_selectrows_old_album->description;
 $db_selectrows_old_album_album_cover_item_id = $db_selectrows_old_album->album_cover_item_id;
 $db_selectrows_old_album_level = $db_selectrows_old_album->level;
 
-if ($db_selectrows_new_album_folder == $db_selectrows_old_album_relative_path_cache) {
 
-$db_select_old_album_album_cover_item_id = mysqli_query($dbconnect_old, "SELECT `id`,`relative_path_cache` FROM `items` WHERE `id`=$db_selectrows_old_album_album_cover_item_id");
-
+$db_select_old_album_album_cover_item_id = mysqli_query($dbconnect_old, "SELECT `id`,`relative_path_cache` FROM `items` WHERE `id`='" . $db_selectrows_old_album_album_cover_item_id . "'");
 $db_selectrows_old_album_thumb = mysqli_fetch_object($db_select_old_album_album_cover_item_id);
 $old_album_thumb_path = $db_selectrows_old_album_thumb->relative_path_cache;
 $new_album_thumb_path = "/" . $old_album_thumb_path;
 
 
-$db_select_old_album_level = mysqli_query($dbconnect_old, "SELECT `weight`,`relative_path_cache` FROM `items` WHERE `level`=$db_selectrows_old_album_level");
+$db_select_old_album_level = mysqli_query($dbconnect_old, "SELECT `weight`,`relative_path_cache` FROM `items` WHERE `level`='" . $db_selectrows_old_album_level . "'");
 
 $old_album_weights_array = array();
 while($db_select_old_album_level_weight = mysqli_fetch_object($db_select_old_album_level)) {
 $old_album_weight = $db_select_old_album_level_weight->weight;
 $old_album_relative_path_cache = $db_select_old_album_level_weight->relative_path_cache;
 
-if (strpos($db_selectrows_old_album_relative_path_cache, "/")){
-$old_album_relative_path_cache_short = str_replace(strrchr($db_selectrows_old_album_relative_path_cache, "/"), "", $db_selectrows_old_album_relative_path_cache);
-if (strpos($old_album_relative_path_cache, $old_album_relative_path_cache_short) === 0) {
-$old_album_weights_array[$old_album_weight] = $old_album_relative_path_cache;
+$old_album_relative_path_cache = $old_album_relative_path_cache;
+
+if (strpos($old_album_relative_path_cache, "/")) {
+if (str_replace(strrchr($old_album_relative_path_cache, "/"), "", $old_album_relative_path_cache) == str_replace(strrchr($db_selectrows_new_album_folder, "/"), "", $db_selectrows_new_album_folder)) {
+$old_album_weights_array[$old_album_relative_path_cache] = $old_album_weight;
 }
 } else {
-$old_album_weights_array[$old_album_weight] = $old_album_relative_path_cache;
+$old_album_weights_array[$old_album_relative_path_cache] = $old_album_weight;
 }
 }
 
-ksort($old_album_weights_array, SORT_NUMERIC);
-$new_album_weights_array = array_values($old_album_weights_array);
-$new_sort_order = array_search($db_selectrows_new_album_folder, $new_album_weights_array);
+asort($old_album_weights_array, SORT_NUMERIC);
+$new_album_weights_array = array();
+$o = 0;
+foreach ($old_album_weights_array as $key => $value) {
+$new_album_weights_array[$key] = $o;
+$o++;
+}
+
+$new_sort_order = $new_album_weights_array[$db_selectrows_new_album_folder];
 
 echo "\n<br>";
 echo "\n<br>zenphoto id: ";
@@ -81,16 +90,13 @@ echo "<br><br>\n\n";
 $database_update_albums = "UPDATE albums SET `title`='" . mysqli_real_escape_string($dbconnect_new, $db_selectrows_old_album_title) . "', `desc`='" . mysqli_real_escape_string($dbconnect_new, $db_selectrows_old_album_desc) . "', `thumb`='" . $new_album_thumb_path . "', `sort_order`='" . $new_sort_order . "', `sort_type`='manual' WHERE `id`='" . $db_selectrows_new_album_id . "'";
 
 if ($dbconnect_new->query($database_update_albums) === TRUE) {
-    echo "Update successfull";
+    echo "Update successfull<br><br>\n\n";
 } else {
-    echo "Update error: " . $dbconnect_new->error;
-}
-
-echo "<br><br>\n\n";
-
+    echo "Update error: " . $dbconnect_new->error .  "<br><br>\n\n";
 }
 
 }
+//if ($i > 10) { break; }
 }
 
 mysqli_close($dbconnect_old);
